060fcb5a5934fb871f8a3b0f939b9cc1ec7f8aa4
Tennent <zalan.marton@gmail.com>
2024-09-24 11:13:51 +0200

    Define function to clean up html body

--- scrape.py
+++ scrape.py
@@ -27,3 +27,20 @@ def extract_body_content(html_content):
     if body_content:
         return str(body_content)
     return ""
+
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    # Get text from body and seperate by line
+    cleaned_content = soup.get_text(separator="\n")
+
+    # Remove unnecessary newlines
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
